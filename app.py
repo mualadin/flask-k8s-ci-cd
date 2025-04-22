ce0411f08656bdd1df3c462ad240047180acf722
root <root@t1-k8snode-01.tfo.local>
2025-04-21 21:54:11 -0400

    Another version added

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
    # return f"Hello from Flask! ENV: {os.getenv('APP_ENV')}, SECRET: {os.getenv('SECRET_KEY')}"
-   return f"<h1>🚀 CI/CD(using argoCD and gitops) CI  is working!</h1><p>ENV: {os.environ.get('ENV', 'development')}</p><p>SECRET: {os.environ.get('SECRET', 'notset')}</p>"
+   return f"<h1>🚀 CI/CD with argoCD and  CI  is working!</h1><p>ENV: {os.environ.get('ENV', 'development')}</p><p>SECRET: {os.environ.get('SECRET', 'notset')}</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
